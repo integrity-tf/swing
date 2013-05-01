0e46cda6cf35fb116cbc0354f3233ef25b1d9a0e
Rene Schneider <rene.schneider@gebit.de>
2013-05-01 23:20:58 +0200

    fixed comments

--- de.gebit.integrity.bindings.swing.eclipse/src/de/gebit/integrity/bindings/swing/eclipse/tables/SwingTableContentFixtureParameterEnumerator.java
+++ de.gebit.integrity.bindings.swing.eclipse/src/de/gebit/integrity/bindings/swing/eclipse/tables/SwingTableContentFixtureParameterEnumerator.java
@@ -1,6 +1,10 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2013 Rene Schneider, GEBIT Solutions GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package de.gebit.integrity.bindings.swing.eclipse.tables;
 
 import java.io.BufferedReader;
@@ -15,7 +19,8 @@ import de.gebit.integrity.fixtures.ArbitraryParameterEnumerator;
 import de.gebit.integrity.fixtures.ArbitraryParameterFixtureLink;
 
 /**
- * 
+ * This parameter enumerator parses data from the swing author assist server and produces proposals for arbitrary
+ * parameters in JTable tests, basically resembling the columns of the table.
  * 
  * @author Slartibartfast
  * 
