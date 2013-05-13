16a410bbb7a0f323eaf2d2f9cbc2755a6004b517
Rene Schneider <rene.schneider@gebit.de>
2013-05-13 16:49:57 +0200

    added more swing application startup options

--- de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/launch/SwingApplicationLaunchFixture.java
+++ de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/launch/SwingApplicationLaunchFixture.java
@@ -7,17 +7,23 @@
  *******************************************************************************/
 package de.gebit.integrity.bindings.swing.launch;
 
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Window;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 import javax.swing.JFrame;
 
+import de.gebit.integrity.bindings.swing.AbstractSwingComponentHandler;
+import de.gebit.integrity.fixtures.FixtureMethod;
+import de.gebit.integrity.fixtures.FixtureParameter;
 import de.gebit.integrity.runner.fixtures.JavaApplicationLaunchFixture;
 
 /**
- * Swing applications are a bit...special. I need a special launching fixture for those...
+ * Swing applications are a bit...special. I need a special launching fixture
+ * for those...
  * 
  * @author Rene Schneider - initial API and implementation
  * 
@@ -30,31 +36,95 @@ public class SwingApplicationLaunchFixture extends JavaApplicationLaunchFixture 
 	private static final long DEFAULT_FRAME_TIMEOUT = 30000;
 
 	/**
-	 * The default number of frames to wait for until the application is considered "alive".
+	 * The default timeout to wait for a requested component to appear.
+	 */
+	private static final long DEFAULT_COMPONENT_TIMEOUT = 30000;
+
+	/**
+	 * The default number of frames to wait for until the application is
+	 * considered "alive".
 	 */
 	private static final int DEFAULT_FRAME_COUNT = 1;
 
 	/**
-	 * Synchronization object used to wait for the app to start.
+	 * Launches the provided application by calling the static main method of
+	 * the application class and waiting the provided number of milliseconds to
+	 * give the application some time to fully start up. This start method is
+	 * timing-sensitive and should only be used as a last resort, if calling
+	 * {@link #launch(String, String[])} and
+	 * {@link #launchAndWaitForComponent(String, String[], String)} are not
+	 * sufficient!
+	 * 
+	 * @param aMainClassName
+	 *            the name of the application class
+	 * @param someArguments
+	 *            the arguments to provide to the main method
+	 * @param aTimeToWait
+	 *            the number of milliseconds to wait
+	 * @return an application wrapper instance which can optionally be saved to
+	 *         handle multiple applications
+	 * @throws Throwable
 	 */
-	private final Object startupSync = new Object();
+	@FixtureMethod(description = "Launch Java Swing Application '$mainClass$' and wait $time$ milliseconds.")
+	public ApplicationWrapper launchAndWait(
+			@FixtureParameter(name = "mainClass") String aMainClassName,
+			@FixtureParameter(name = "arguments") String[] someArguments,
+			@FixtureParameter(name = "time") int aTimeToWait) throws Throwable {
+		ApplicationWrapper tempWrapper = super.launch(aMainClassName,
+				someArguments);
 
-	/**
-	 * Whether the application is considered "started".
-	 */
-	private boolean startupSuccessful;
-
-	@Override
-	protected boolean checkWrapper(ApplicationWrapper aWrapper) throws Throwable {
-		FrameWaiter tempWaiter = new FrameWaiter(getFrameTimeout(), getFrameCount());
-
-		synchronized (startupSync) {
-			tempWaiter.start();
-
-			startupSync.wait();
+		if (aTimeToWait > 0) {
+			try {
+				Thread.sleep(aTimeToWait);
+			} catch (InterruptedException exc) {
+				// ignore
+			}
 		}
 
-		return startupSuccessful;
+		return tempWrapper;
+	}
+
+	/**
+	 * Launches the provided application by calling the static main method of
+	 * the application class and waiting for a specified component to appear.
+	 * 
+	 * @param aMainClassName
+	 *            the name of the application class
+	 * @param someArguments
+	 *            the arguments to provide to the main method
+	 * @param aComponentPath
+	 *            the component to wait for
+	 * @return an application wrapper instance which can optionally be saved to
+	 *         handle multiple applications
+	 * @throws Throwable
+	 */
+	@FixtureMethod(description = "Launch Java Swing Application '$mainClass$' and wait for component '$component$' to appear.")
+	public ApplicationWrapper launchAndWaitForComponent(
+			@FixtureParameter(name = "mainClass") String aMainClassName,
+			@FixtureParameter(name = "arguments") String[] someArguments,
+			@FixtureParameter(name = "component") String aComponentPath)
+			throws Throwable {
+		ApplicationWrapper tempWrapper = super.launch(aMainClassName,
+				someArguments);
+
+		if (aComponentPath != null) {
+			ComponentWaiter tempWaiter = new ComponentWaiter(
+					getComponentTimeout(), aComponentPath);
+			tempWaiter.start();
+			tempWaiter.wasSuccessful();
+		}
+
+		return tempWrapper;
+	}
+
+	@Override
+	protected boolean checkWrapper(ApplicationWrapper aWrapper)
+			throws Throwable {
+		FrameWaiter tempWaiter = new FrameWaiter(getFrameTimeout(),
+				getFrameCount());
+
+		tempWaiter.start();
+		return tempWaiter.wasSuccessful();
 	}
 
 	@Override
@@ -80,7 +150,8 @@ public class SwingApplicationLaunchFixture extends JavaApplicationLaunchFixture 
 		// writing all results.
 		for (Window tempWindow : Window.getWindows()) {
 			if (tempWindow instanceof JFrame) {
-				((JFrame) tempWindow).setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				((JFrame) tempWindow)
+						.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			}
 			tempWindow.dispose();
 		}
@@ -90,6 +161,10 @@ public class SwingApplicationLaunchFixture extends JavaApplicationLaunchFixture 
 
 	protected long getFrameTimeout() {
 		return DEFAULT_FRAME_TIMEOUT;
+	}
+
+	protected long getComponentTimeout() {
+		return DEFAULT_COMPONENT_TIMEOUT;
 	}
 
 	protected int getFrameCount() {
@@ -116,6 +191,11 @@ public class SwingApplicationLaunchFixture extends JavaApplicationLaunchFixture 
 		private int numberOfFrames;
 
 		/**
+		 * Whether the waiting is considered successful.
+		 */
+		private boolean waitSuccessful;
+
+		/**
 		 * Creates an instance.
 		 */
 		public FrameWaiter(long aTimeout, int aNumberOfFrames) {
@@ -129,11 +209,8 @@ public class SwingApplicationLaunchFixture extends JavaApplicationLaunchFixture 
 
 			while (tempStart + timeoutNanos > System.nanoTime()) {
 				if (areFramesVisible() && isEventThreadReactive()) {
-					synchronized (startupSync) {
-						startupSuccessful = true;
-						startupSync.notifyAll();
-						return;
-					}
+					waitSuccessful = true;
+					return;
 				}
 
 				try {
@@ -143,9 +220,7 @@ public class SwingApplicationLaunchFixture extends JavaApplicationLaunchFixture 
 				}
 			}
 
-			synchronized (startupSync) {
-				startupSync.notifyAll();
-			}
+			waitSuccessful = false;
 		}
 
 		/**
@@ -166,30 +241,126 @@ public class SwingApplicationLaunchFixture extends JavaApplicationLaunchFixture 
 		}
 
 		/**
-		 * Checks whether the AWT event thread is reacting and processing messages.
+		 * Wait for the thread to exit and check the success.
 		 * 
-		 * @return true if it is processing messages, false if not.
+		 * @return
 		 */
-		protected boolean isEventThreadReactive() {
+		public boolean wasSuccessful() {
 			try {
-				EventQueue.invokeAndWait(new Runnable() {
-
-					@Override
-					public void run() {
-						return;
-					}
-				});
-
-				return true;
+				join();
 			} catch (InterruptedException exc) {
-				exc.printStackTrace();
-			} catch (InvocationTargetException exc) {
-				exc.printStackTrace();
+				// ignore
 			}
 
-			return false;
+			return waitSuccessful;
 		}
 
+	}
+
+	/**
+	 * Waits for a specific component to come up after launch.
+	 * 
+	 * 
+	 * @author Rene Schneider - initial API and implementation
+	 * 
+	 */
+	protected class ComponentWaiter extends Thread {
+
+		/**
+		 * The nanoseconds to wait for.
+		 */
+		private long timeoutNanos;
+
+		/**
+		 * The components' path.
+		 */
+		private String componentPath;
+
+		/**
+		 * Whether the waiting is considered successful.
+		 */
+		private boolean waitSuccessful;
+
+		/**
+		 * Creates an instance.
+		 */
+		public ComponentWaiter(long aTimeout, String aComponentPath) {
+			timeoutNanos = aTimeout * 1000000L;
+			componentPath = aComponentPath;
+		}
+
+		@Override
+		public void run() {
+			long tempStart = System.nanoTime();
+
+			while (tempStart + timeoutNanos > System.nanoTime()) {
+				if (componentExists() && isEventThreadReactive()) {
+					waitSuccessful = true;
+					return;
+				}
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException exc) {
+					// ignored
+				}
+			}
+
+			waitSuccessful = false;
+		}
+
+		/**
+		 * Checks whether the component to be found exists.
+		 * 
+		 * @return true or false
+		 */
+		protected boolean componentExists() {
+			List<Component> tempList = new AbstractSwingComponentHandler() {
+			}.findComponents(componentPath, null, null);
+
+			return !(tempList.isEmpty());
+		}
+
+		/**
+		 * Wait for the thread to exit and check the success.
+		 * 
+		 * @return
+		 */
+		public boolean wasSuccessful() {
+			try {
+				join();
+			} catch (InterruptedException exc) {
+				// ignore
+			}
+
+			return waitSuccessful;
+		}
+
+	}
+
+	/**
+	 * Checks whether the AWT event thread is reacting and processing messages.
+	 * 
+	 * @return true if it is processing messages, false if not.
+	 */
+	protected boolean isEventThreadReactive() {
+		try {
+			EventQueue.invokeAndWait(new Runnable() {
+
+				@Override
+				public void run() {
+					return;
+				}
+			});
+
+			return true;
+		} catch (InterruptedException exc) {
+			exc.printStackTrace();
+		} catch (InvocationTargetException exc) {
+			exc.printStackTrace();
+		}
+
+		return false;
 	}
 
 }
