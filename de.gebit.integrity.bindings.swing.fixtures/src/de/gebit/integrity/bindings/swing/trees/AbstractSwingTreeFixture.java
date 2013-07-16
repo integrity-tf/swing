208b8ecda1eae9e09e1c38ce3b851e040eb5305d
Rene Schneider <rene.schneider@gebit.de>
2013-07-16 11:17:15 +0200

    first attempt at a complete tree fixture

--- de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/trees/AbstractSwingTreeFixture.java
+++ de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/trees/AbstractSwingTreeFixture.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
@@ -47,7 +48,8 @@ public abstract class AbstractSwingTreeFixture<C extends JTree> extends Abstract
 	/**
 	 * The names of methods for which only parent nodes shall be suggested for {@link #TREE_ITEM_PATH_PARAMETER_NAME}.
 	 */
-	public static final String[] TREE_ITEM_PARENTS_ONLY_METHODS = new String[] {};
+	public static final String[] TREE_ITEM_PARENTS_ONLY_METHODS = new String[] { "expandEntry", "collapseEntry",
+			"isEntryExpanded" };
 
 	/**
 	 * Parses a serialized path into a {@link TreePath} object. When parsing, '^' chars are used as delimiters of
@@ -126,6 +128,32 @@ public abstract class AbstractSwingTreeFixture<C extends JTree> extends Abstract
 	}
 
 	/**
+	 * Generates a textual tree path representation for the provided path.
+	 * 
+	 * @param aPath
+	 *            the path to use
+	 * @return the string representation of the path
+	 */
+	protected String generateTreePathString(TreePath aPath) {
+		StringBuilder tempBuilder = new StringBuilder();
+		for (int i = 0; i < aPath.getPathCount(); i++) {
+			Object tempPathComponent = aPath.getPathComponent(i);
+
+			String tempChildPathPart = tempPathComponent.toString();
+
+			// Escape this childs' name, if necessary
+			tempChildPathPart = tempChildPathPart.replace("^", "\\^");
+
+			if (i > 0) {
+				tempBuilder.append("^");
+			}
+			tempBuilder.append(tempChildPathPart);
+		}
+
+		return tempBuilder.toString();
+	}
+
+	/**
 	 * Checks whether a given (partial) path string matches a given {@link TreeNode}.
 	 * 
 	 * @param aPathPart
@@ -139,6 +167,36 @@ public abstract class AbstractSwingTreeFixture<C extends JTree> extends Abstract
 	}
 
 	/**
+	 * This determines the textual representation of a given tree node.
+	 * 
+	 * @param aPath
+	 *            the path to look at
+	 * @return the textual value
+	 */
+	protected String getTextOfNodeAtPath(TreePath aPath) {
+		TreeNode tempNode = (TreeNode) aPath.getLastPathComponent();
+		return tempNode.toString();
+	}
+
+	/**
+	 * This determines the value of a given tree node. The value is a user-specified object in case a
+	 * {@link DefaultMutableTreeNode} is used, or a string in case an other node is used.
+	 * 
+	 * @param aPath
+	 *            the path to look at
+	 * @return the value
+	 */
+	protected Object getValueOfNodeAtPath(TreePath aPath) {
+		TreeNode tempNode = (TreeNode) aPath.getLastPathComponent();
+
+		if (tempNode instanceof DefaultMutableTreeNode) {
+			return ((DefaultMutableTreeNode) tempNode).getUserObject();
+		} else {
+			return tempNode.toString();
+		}
+	}
+
+	/**
 	 * Gets the number of columns from the given table.
 	 * 
 	 * @param aComponentPath
@@ -147,22 +205,224 @@ public abstract class AbstractSwingTreeFixture<C extends JTree> extends Abstract
 	 * @throws EventQueueTimeoutException
 	 * @throws InvalidComponentPathException
 	 */
-	@FixtureMethod(descriptionCall = "Get the number of {" + TREE_ITEM_PATH_PARAMETER_NAME + "?childs of item '$"
+	@FixtureMethod(descriptionCall = "Get the number of {" + TREE_ITEM_PATH_PARAMETER_NAME + "?childs of entry '$"
 			+ TREE_ITEM_PATH_PARAMETER_NAME + "$'}{^" + TREE_ITEM_PATH_PARAMETER_NAME
 			+ "?first-level items} in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'", descriptionTest = "Check the number of {"
 			+ TREE_ITEM_PATH_PARAMETER_NAME
-			+ "?childs of item '$"
+			+ "?childs of entry '$"
 			+ TREE_ITEM_PATH_PARAMETER_NAME
 			+ "$'}{^"
 			+ TREE_ITEM_PATH_PARAMETER_NAME + "?first-level items} in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'")
-	public Integer getTreeChildCount(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+	public Integer getChildCount(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
 			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath)
 			throws AmbiguousComponentPathException, EventQueueTimeoutException, InvalidComponentPathException {
 		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
-
 		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
 
 		return ((TreeNode) tempPath.getLastPathComponent()).getChildCount();
 	}
 
+	/**
+	 * Get the value of a specific tree node entry. This returns the user object in case of a
+	 * {@link DefaultMutableTreeNode}, or the textual string in case of any other kind of tree node.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @param anItemPath
+	 *            the tree entries' path
+	 * @return the value of the entry
+	 */
+	@FixtureMethod(descriptionCall = "Get the value of entry '$" + TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$"
+			+ COMPONENT_PATH_PARAMETER_NAME + "$'", descriptionTest = "Check the value of entry '$"
+			+ TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'")
+	public Object getEntryValue(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
+
+		return getValueOfNodeAtPath(tempPath);
+	}
+
+	/**
+	 * Get the text of a specific tree node entry.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @param anItemPath
+	 *            the tree entries' path
+	 * @return the textual value of the entry
+	 */
+	@FixtureMethod(descriptionCall = "Get the text of entry '$" + TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$"
+			+ COMPONENT_PATH_PARAMETER_NAME + "$'", descriptionTest = "Check the text of entry '$"
+			+ TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'")
+	public Object getEntryText(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
+
+		return getTextOfNodeAtPath(tempPath);
+	}
+
+	/**
+	 * Expands an entry in a tree. If parents of this entry aren't expanded yet, they will be expanded automatically as
+	 * well.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @param anItemPath
+	 *            the tree entries' path
+	 */
+	@FixtureMethod(description = "Expand entry '$" + TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$"
+			+ COMPONENT_PATH_PARAMETER_NAME + "$'")
+	public void expandEntry(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
+
+		tempTree.expandPath(tempPath);
+	}
+
+	/**
+	 * Collapses an entry in a tree.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @param anItemPath
+	 *            the tree entries' path
+	 */
+	@FixtureMethod(description = "Collapse entry '$" + TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$"
+			+ COMPONENT_PATH_PARAMETER_NAME + "$'")
+	public void collapseEntry(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
+
+		tempTree.collapsePath(tempPath);
+	}
+
+	/**
+	 * Checks whether a given entry in a tree is currently expanded.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @param anItemPath
+	 *            the tree entries' path
+	 */
+	@FixtureMethod(descriptionCall = "Get the expanded state of entry '$" + TREE_ITEM_PATH_PARAMETER_NAME
+			+ "$' in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'", descriptionTest = "Check whether entry '$"
+			+ TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$' is expanded")
+	public Boolean isEntryExpanded(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
+
+		return tempTree.isExpanded(tempPath);
+	}
+
+	/**
+	 * Checks whether a given entry in a tree is currently visible. An entry is considered visible if its parents are
+	 * expanded.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @param anItemPath
+	 *            the tree entries' path
+	 */
+	@FixtureMethod(descriptionCall = "Get the visibility state of entry '$" + TREE_ITEM_PATH_PARAMETER_NAME
+			+ "$' in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'", descriptionTest = "Check whether entry '$"
+			+ TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$' is visible")
+	public Boolean isEntryVisible(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
+
+		return tempTree.isVisible(tempPath);
+	}
+
+	/**
+	 * Selects one or multiple entries in the given tree.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @param someEntryPaths
+	 *            one or multiple paths to tree entries
+	 */
+	@FixtureMethod(description = "Select entry '$" + TREE_ITEM_PATH_PARAMETER_NAME + "$' in tree '$"
+			+ COMPONENT_PATH_PARAMETER_NAME + "$'")
+	public void selectEntries(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String[] someEntryPaths) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+
+		if (someEntryPaths == null || someEntryPaths.length == 0) {
+			throw new IllegalArgumentException("At least one tree entry path must be provided.");
+		}
+
+		TreePath[] tempPaths = new TreePath[someEntryPaths.length];
+		for (int i = 0; i < someEntryPaths.length; i++) {
+			tempPaths[i] = parseTreePathString(tempTree, someEntryPaths[i]);
+		}
+
+		tempTree.setSelectionPaths(tempPaths);
+	}
+
+	/**
+	 * Returns the paths of the selected entries in a given tree.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @return the paths of the selected entries
+	 */
+	@FixtureMethod(descriptionCall = "Get the paths of the selected entries in tree '$" + COMPONENT_PATH_PARAMETER_NAME
+			+ "$'")
+	public String[] getSelectedEntriesPath(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+
+		ArrayList<String> tempPathStrings = new ArrayList<String>();
+		for (TreePath tempPath : tempTree.getSelectionPaths()) {
+			tempPathStrings.add(generateTreePathString(tempPath));
+		}
+
+		return tempPathStrings.toArray(new String[tempPathStrings.size()]);
+	}
+
+	/**
+	 * Returns the textual representations of the selected entries in a given tree.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @return the paths of the selected entries
+	 */
+	@FixtureMethod(descriptionCall = "Get the texts of the selected entries in tree '$" + COMPONENT_PATH_PARAMETER_NAME
+			+ "$'")
+	public String[] getSelectedEntriesText(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+
+		ArrayList<String> tempStrings = new ArrayList<String>();
+		for (TreePath tempPath : tempTree.getSelectionPaths()) {
+			tempStrings.add(getTextOfNodeAtPath(tempPath));
+		}
+
+		return tempStrings.toArray(new String[tempStrings.size()]);
+	}
+
+	/**
+	 * Returns the textual representations of the selected entries in a given tree.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @return the paths of the selected entries
+	 */
+	@FixtureMethod(descriptionCall = "Get the values of the selected entries in tree '$"
+			+ COMPONENT_PATH_PARAMETER_NAME + "$'")
+	public Object[] getSelectedEntriesValue(
+			@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath) {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+
+		ArrayList<Object> tempObjects = new ArrayList<Object>();
+		for (TreePath tempPath : tempTree.getSelectionPaths()) {
+			tempObjects.add(getValueOfNodeAtPath(tempPath));
+		}
+
+		return tempObjects.toArray(new Object[tempObjects.size()]);
+	}
 }
