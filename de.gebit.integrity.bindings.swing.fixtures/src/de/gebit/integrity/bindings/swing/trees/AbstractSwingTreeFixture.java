d295f744ada867a36768eefe70cdcd3b775b42db
Rene Schneider <rene.schneider@gebit.de>
2013-07-16 09:40:20 +0200

    abstracted out everything from SwingTreeFixture

--- de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/trees/AbstractSwingTreeFixture.java
+++ de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/trees/AbstractSwingTreeFixture.java
@@ -15,15 +15,24 @@ import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
 import de.gebit.integrity.bindings.swing.AbstractSwingFixture;
+import de.gebit.integrity.bindings.swing.exceptions.AmbiguousComponentPathException;
+import de.gebit.integrity.bindings.swing.exceptions.EventQueueTimeoutException;
+import de.gebit.integrity.bindings.swing.exceptions.InvalidComponentPathException;
 import de.gebit.integrity.fixtures.CustomProposalFixture;
+import de.gebit.integrity.fixtures.FixtureMethod;
+import de.gebit.integrity.fixtures.FixtureParameter;
 
 /**
  * This fixture is an abstract base for fixtures providing access to JTree components.
  * 
+ * @param <C>
+ *            the component
+ * 
  * @author Rene Schneider - initial API and implementation
  * 
  */
-public abstract class AbstractSwingTreeFixture extends AbstractSwingFixture implements CustomProposalFixture {
+public abstract class AbstractSwingTreeFixture<C extends JTree> extends AbstractSwingFixture<C> implements
+		CustomProposalFixture {
 
 	/**
 	 * The name of the parameter denoting the path to individual tree items.
@@ -127,6 +136,33 @@ public abstract class AbstractSwingTreeFixture extends AbstractSwingFixture impl
 	 */
 	protected boolean checkPathPartEquality(String aPathPart, TreeNode aNode) {
 		return aPathPart.equals(aNode.toString());
+	}
+
+	/**
+	 * Gets the number of columns from the given table.
+	 * 
+	 * @param aComponentPath
+	 *            the path to the component
+	 * @throws AmbiguousComponentPathException
+	 * @throws EventQueueTimeoutException
+	 * @throws InvalidComponentPathException
+	 */
+	@FixtureMethod(descriptionCall = "Get the number of {" + TREE_ITEM_PATH_PARAMETER_NAME + "?childs of item '$"
+			+ TREE_ITEM_PATH_PARAMETER_NAME + "$'}{^" + TREE_ITEM_PATH_PARAMETER_NAME
+			+ "?first-level items} in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'", descriptionTest = "Check the number of {"
+			+ TREE_ITEM_PATH_PARAMETER_NAME
+			+ "?childs of item '$"
+			+ TREE_ITEM_PATH_PARAMETER_NAME
+			+ "$'}{^"
+			+ TREE_ITEM_PATH_PARAMETER_NAME + "?first-level items} in tree '$" + COMPONENT_PATH_PARAMETER_NAME + "$'")
+	public Integer getTreeChildCount(@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
+			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath)
+			throws AmbiguousComponentPathException, EventQueueTimeoutException, InvalidComponentPathException {
+		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
+
+		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
+
+		return ((TreeNode) tempPath.getLastPathComponent()).getChildCount();
 	}
 
 }
--- de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/trees/SwingTreeFixture.java
+++ de.gebit.integrity.bindings.swing.fixtures/src/de/gebit/integrity/bindings/swing/trees/SwingTreeFixture.java
@@ -8,15 +8,8 @@
 package de.gebit.integrity.bindings.swing.trees;
 
 import javax.swing.JTree;
-import javax.swing.tree.TreeNode;
-import javax.swing.tree.TreePath;
 
-import de.gebit.integrity.bindings.swing.exceptions.AmbiguousComponentPathException;
-import de.gebit.integrity.bindings.swing.exceptions.EventQueueTimeoutException;
-import de.gebit.integrity.bindings.swing.exceptions.InvalidComponentPathException;
 import de.gebit.integrity.fixtures.CustomProposalFixture;
-import de.gebit.integrity.fixtures.FixtureMethod;
-import de.gebit.integrity.fixtures.FixtureParameter;
 
 /**
  * This fixture provides access to {@link JTree} components.
@@ -24,39 +17,6 @@ import de.gebit.integrity.fixtures.FixtureParameter;
  * @author Rene Schneider - initial API and implementation
  * 
  */
-public class SwingTreeFixture extends AbstractSwingTreeFixture implements
-		CustomProposalFixture {
+public class SwingTreeFixture extends AbstractSwingTreeFixture<JTree> implements CustomProposalFixture {
 
-	/**
-	 * Gets the number of columns from the given table.
-	 * 
-	 * @param aComponentPath
-	 *            the path to the component
-	 * @throws AmbiguousComponentPathException
-	 * @throws EventQueueTimeoutException
-	 * @throws InvalidComponentPathException
-	 */
-	@FixtureMethod(descriptionCall = "Get the number of {"
-			+ TREE_ITEM_PATH_PARAMETER_NAME + "?childs of item '$"
-			+ TREE_ITEM_PATH_PARAMETER_NAME + "$'}{^"
-			+ TREE_ITEM_PATH_PARAMETER_NAME + "?first-level items} in tree '$"
-			+ COMPONENT_PATH_PARAMETER_NAME + "$'", descriptionTest = "Check the number of {"
-			+ TREE_ITEM_PATH_PARAMETER_NAME
-			+ "?childs of item '$"
-			+ TREE_ITEM_PATH_PARAMETER_NAME
-			+ "$'}{^"
-			+ TREE_ITEM_PATH_PARAMETER_NAME
-			+ "?first-level items} in tree '$"
-			+ COMPONENT_PATH_PARAMETER_NAME + "$'")
-	public Integer getTreeChildCount(
-			@FixtureParameter(name = COMPONENT_PATH_PARAMETER_NAME) String aComponentPath,
-			@FixtureParameter(name = TREE_ITEM_PATH_PARAMETER_NAME) String anItemPath)
-			throws AmbiguousComponentPathException, EventQueueTimeoutException,
-			InvalidComponentPathException {
-		JTree tempTree = findComponentGuarded(aComponentPath, JTree.class, null);
-
-		TreePath tempPath = parseTreePathString(tempTree, anItemPath);
-
-		return ((TreeNode) tempPath.getLastPathComponent()).getChildCount();
-	}
 }
